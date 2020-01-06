56b58208d6ba194d4a9b874849563ff2083b10a1
Grégoire HUBERT <hubert.greg@gmail.com>
2020-01-06 20:44:07 +0100

    add CLD instruction

---
+++ src/cpu_instruction/microcode/cld.rs
@@ -0,0 +1,30 @@
+use super::*;
+
+pub fn cld(memory: &mut Memory, registers: &mut Registers, cpu_instruction: &CPUInstruction) -> Result<LogLine> {
+    let resolution = cpu_instruction.addressing_mode
+        .solve(registers.command_pointer, memory, registers)?;
+
+    registers.set_d_flag(false);
+    registers.command_pointer += 1 + resolution.operands.len();
+
+    Ok(LogLine::new(&cpu_instruction, resolution, format!("[S={}]", registers.format_status())))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::cpu_instruction::cpu_instruction::tests::get_stuff;
+
+    #[test]
+    fn test_cld() {
+        let cpu_instruction = CPUInstruction::new(0x1000, 0xca, "CLD", AddressingMode::Implied, cld);
+        let (mut memory, mut registers) = get_stuff(0x1000, vec![0x4c, 0x0a, 0x02]);
+        registers.set_d_flag(true);
+        let log_line = cpu_instruction.execute(&mut memory, &mut registers).unwrap();
+        assert_eq!("CLD".to_owned(), log_line.mnemonic);
+        assert_eq!(0x1001, registers.command_pointer);
+        assert!(!registers.d_flag_is_set());
+    }
+}
+
+
