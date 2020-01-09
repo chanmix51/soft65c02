cc07b720b9ba5f5bec882a9e4d10c0cbc5df813c
Grégoire HUBERT <hubert.greg@gmail.com>
2020-01-09 12:21:33 +0100

    add SEC, SED, SEI

---
+++ src/cpu_instruction/microcode/sei.rs
@@ -0,0 +1,32 @@
+use super::*;
+
+pub fn sei(memory: &mut Memory, registers: &mut Registers, cpu_instruction: &CPUInstruction) -> Result<LogLine> {
+    let resolution = cpu_instruction.addressing_mode
+        .solve(registers.command_pointer, memory, registers)?;
+
+    registers.set_i_flag(true);
+    registers.command_pointer += 1 + resolution.operands.len();
+
+    Ok(LogLine::new(&cpu_instruction, resolution, format!("[S={}]", registers.format_status())))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::cpu_instruction::cpu_instruction::tests::get_stuff;
+
+    #[test]
+    fn test_sei() {
+        let cpu_instruction = CPUInstruction::new(0x1000, 0xca, "SEI", AddressingMode::Implied, sei);
+        let (mut memory, mut registers) = get_stuff(0x1000, vec![0x4c, 0x0a, 0x02]);
+        registers.set_i_flag(false);
+        let log_line = cpu_instruction.execute(&mut memory, &mut registers).unwrap();
+        assert_eq!("SEI".to_owned(), log_line.mnemonic);
+        assert_eq!(0x1001, registers.command_pointer);
+        assert!(registers.i_flag_is_set());
+    }
+}
+
+
+
+
