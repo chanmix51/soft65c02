723fb8b74e95396704b7b3a59d35ad5f0c587c8d
Grégoire HUBERT <hubert.greg@gmail.com>
2020-01-30 12:01:51 +0100

    fix TSB instruction

--- src/cpu_instruction/microcode/tsb.rs
+++ src/cpu_instruction/microcode/tsb.rs
@@ -14,7 +14,7 @@ pub fn tsb(
         .expect("TSB must have operands, crashing the application");
 
     let byte = memory.read(target_address, 1)?[0];
-    registers.set_z_flag((byte & registers.accumulator != 0) | (registers.accumulator == 0));
+    registers.set_z_flag(byte & registers.accumulator == 0);
     let res = byte | registers.accumulator;
     memory.write(target_address, &vec![res])?;
     registers.command_pointer += 1 + resolution.operands.len();
@@ -22,7 +22,7 @@ pub fn tsb(
     Ok(LogLine::new(
         &cpu_instruction,
         resolution,
-        format!("0x{:02x}[S={}]", res, registers.format_status()),
+        format!("(0x{:02x})[S={}]", res, registers.format_status()),
     ))
 }
 
@@ -31,18 +31,21 @@ mod tests {
     use super::*;
     use crate::cpu_instruction::cpu_instruction::tests::get_stuff;
 
+    /*
+     * Examples come from http://www.6502.org/tutorials/65c02opcodes.html
+     */
     #[test]
     fn test_tsb() {
         let cpu_instruction =
             CPUInstruction::new(0x1000, 0xca, "TSB", AddressingMode::ZeroPage([0x0a]), tsb);
         let (mut memory, mut registers) = get_stuff(0x1000, vec![0x8a, 0x0a, 0x02]);
-        memory.write(0x0a, &vec![0x55]).unwrap();
-        registers.accumulator = 0xaa;
+        memory.write(0x0a, &vec![0xa6]).unwrap();
+        registers.accumulator = 0x33;
         let log_line = cpu_instruction
             .execute(&mut memory, &mut registers)
             .unwrap();
         assert_eq!("TSB".to_owned(), log_line.mnemonic);
-        assert_eq!(0xff, memory.read(0x0a, 1).unwrap()[0]);
+        assert_eq!(0xb7, memory.read(0x0a, 1).unwrap()[0]);
         assert!(!registers.z_flag_is_set());
         assert_eq!(0x1002, registers.command_pointer);
     }
@@ -52,26 +55,12 @@ mod tests {
         let cpu_instruction =
             CPUInstruction::new(0x1000, 0xca, "TSB", AddressingMode::ZeroPage([0x0a]), tsb);
         let (mut memory, mut registers) = get_stuff(0x1000, vec![0x8a, 0x0a, 0x02]);
-        memory.write(0x0a, &vec![0xf0]).unwrap();
-        registers.accumulator = 0x8f;
+        memory.write(0x0a, &vec![0xa6]).unwrap();
+        registers.accumulator = 0x41;
         let log_line = cpu_instruction
             .execute(&mut memory, &mut registers)
             .unwrap();
-        assert_eq!(0xff, memory.read(0x0a, 1).unwrap()[0]);
-        assert!(registers.z_flag_is_set());
-        assert_eq!(0x1002, registers.command_pointer);
-    }
-
-    #[test]
-    fn test_tsb_with_zero_accu() {
-        let cpu_instruction =
-            CPUInstruction::new(0x1000, 0xca, "TSB", AddressingMode::ZeroPage([0x0a]), tsb);
-        let (mut memory, mut registers) = get_stuff(0x1000, vec![0x8a, 0x0a, 0x02]);
-        memory.write(0x0a, &vec![0x0f]).unwrap();
-        let log_line = cpu_instruction
-            .execute(&mut memory, &mut registers)
-            .unwrap();
-        assert_eq!(0x0f, memory.read(0x0a, 1).unwrap()[0]);
+        assert_eq!(0xe7, memory.read(0x0a, 1).unwrap()[0]);
         assert!(registers.z_flag_is_set());
         assert_eq!(0x1002, registers.command_pointer);
     }
