3d2079ae6d370954cb5fc166e1e22ebfac5bf79b
Grégoire HUBERT <hubert.greg@gmail.com>
2020-01-30 11:55:13 +0100

    fix TRB instruction

--- src/cpu_instruction/microcode/trb.rs
+++ src/cpu_instruction/microcode/trb.rs
@@ -13,23 +13,20 @@ pub fn trb(
         .target_address
         .expect("TRB must have operands, crashing the application");
 
-    let byte = memory.read(target_address, 1)?[0];
-    let res = if byte & registers.accumulator != 0 {
-        registers.set_z_flag(true);
-        let neg = registers.accumulator ^ 0xff; // negates all bits
-        let res = byte & neg;
-        memory.write(target_address, &vec![res])?;
-        res
+    let mut byte = memory.read(target_address, 1)?[0];
+    if byte & registers.accumulator != 0 {
+        byte = byte & (registers.accumulator ^ 0xff);
+        memory.write(target_address, &vec![byte])?;
+        registers.set_z_flag(false);
     } else {
-        registers.set_z_flag(registers.accumulator == 0);
-        byte
-    };
+        registers.set_z_flag(true);
+    }
     registers.command_pointer += 1 + resolution.operands.len();
 
     Ok(LogLine::new(
         &cpu_instruction,
         resolution,
-        format!("0x{:02x}[S={}]", res, registers.format_status()),
+        format!("(0x{:02x})[S={}]", byte, registers.format_status()),
     ))
 }
 
@@ -38,49 +35,39 @@ mod tests {
     use super::*;
     use crate::cpu_instruction::cpu_instruction::tests::get_stuff;
 
+    /*
+     * Examples come from http://www.6502.org/tutorials/65c02opcodes.html
+     */
     #[test]
     fn test_trb() {
         let cpu_instruction =
-            CPUInstruction::new(0x1000, 0xca, "TRB", AddressingMode::ZeroPage([0x0a]), trb);
-        let (mut memory, mut registers) = get_stuff(0x1000, vec![0x8a, 0x0a, 0x02]);
-        memory.write(0x0a, &vec![0xff]).unwrap();
-        registers.accumulator = 0x81;
+            CPUInstruction::new(0x1000, 0xca, "TRB", AddressingMode::ZeroPage([0x00]), trb);
+        let (mut memory, mut registers) = get_stuff(0x1000, vec![0x8a, 0x00, 0x02]);
+        memory.write(0x00, &vec![0xa6]).unwrap();
+        registers.accumulator = 0x33;
         let log_line = cpu_instruction
             .execute(&mut memory, &mut registers)
             .unwrap();
         assert_eq!("TRB".to_owned(), log_line.mnemonic);
-        assert_eq!(0b01111110, memory.read(0x0a, 1).unwrap()[0]);
-        assert!(registers.z_flag_is_set());
-        assert_eq!(0x1002, registers.command_pointer);
-    }
-
-    #[test]
-    fn test_trb_with_no_z_flag() {
-        let cpu_instruction =
-            CPUInstruction::new(0x1000, 0xca, "TRB", AddressingMode::ZeroPage([0x0a]), trb);
-        let (mut memory, mut registers) = get_stuff(0x1000, vec![0x8a, 0x0a, 0x02]);
-        memory.write(0x0a, &vec![0x0f]).unwrap();
-        registers.accumulator = 0x80;
-        let log_line = cpu_instruction
-            .execute(&mut memory, &mut registers)
-            .unwrap();
-        assert_eq!(0x0f, memory.read(0x0a, 1).unwrap()[0]);
+        assert_eq!(0x84, memory.read(0x00, 1).unwrap()[0]);
         assert!(!registers.z_flag_is_set());
+        assert_eq!(0x33, registers.accumulator);
         assert_eq!(0x1002, registers.command_pointer);
     }
 
     #[test]
-    fn test_trb_with_accu_zero() {
+    fn test_trb_with_z_flag() {
         let cpu_instruction =
-            CPUInstruction::new(0x1000, 0xca, "TRB", AddressingMode::ZeroPage([0x0a]), trb);
-        let (mut memory, mut registers) = get_stuff(0x1000, vec![0x8a, 0x0a, 0x02]);
-        memory.write(0x0a, &vec![0x0f]).unwrap();
-        registers.register_y = 0x00;
-        let log_line = cpu_instruction
+            CPUInstruction::new(0x1000, 0xca, "TRB", AddressingMode::ZeroPage([0x00]), trb);
+        let (mut memory, mut registers) = get_stuff(0x1000, vec![0x8a, 0x00, 0x02]);
+        memory.write(0x00, &vec![0xa6]).unwrap();
+        registers.accumulator = 0x41;
+        let _log_line = cpu_instruction
             .execute(&mut memory, &mut registers)
             .unwrap();
-        assert_eq!(0x0f, memory.read(0x0a, 1).unwrap()[0]);
+        assert_eq!(0xa6, memory.read(0x00, 1).unwrap()[0]);
         assert!(registers.z_flag_is_set());
+        assert_eq!(0x41, registers.accumulator);
         assert_eq!(0x1002, registers.command_pointer);
     }
 }
